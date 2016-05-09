507c30179fd389cb91cdc0ec6a9354a67f6d964e
ROODAY <rudytheninja@gmail.com>
2016-05-08 20:08:36 -0400

    added highscores and _blank for article links

--- js/main.js
+++ js/main.js
@@ -5,7 +5,16 @@ var notButton = document.getElementById('not-button');
 var titleElement = document.getElementById('article-title');
 var scoreElement = document.getElementById('score');
 var loaderElement = document.getElementById('loader');
+var highscoreElement = document.getElementById('high-score');
 var number;
+
+if (storageAvailable('localStorage')) {
+	var highscore = localStorage.getItem('onion-highscore');
+	highscoreElement.style.display = "block";
+	if (localStorage.getItem('onion-highscore')) {
+		highscoreElement.innerHTML = "High Score: " + highscore;
+	}
+}
 
 setNewTitle();	
 
@@ -13,16 +22,23 @@ onionButton.addEventListener('click', function() {
 	if (number === 0) {
 		swal({   
 			title: "Good job!",   
-			text: "You got it right! Check out the actual <a href=" + currentArticle.url + ">article</a>!",
+			text: "You got it right! Check out the actual <a target='_blank' href=" + currentArticle.url + ">article</a>!",
 			type: "success",   
 			html: true 
 		}, setNewTitle);
 		score += 1;
+		if (highscore !== undefined) {
+			if (score > highscore) {
+				highscore = score;
+				localStorage.setItem('onion-highscore', highscore);
+				highscoreElement.innerHTML = "High Score: " + highscore;
+			}
+		}	
 		scoreElement.innerHTML = score;
 	} else {
 		swal({   
 			title: "Nope.",   
-			text: "Maybe next time. Check out the actual <a href=" + currentArticle.url + ">article</a>!",
+			text: "Maybe next time. Check out the actual <a target='_blank' href=" + currentArticle.url + ">article</a>!",
 			type: "error",   
 			html: true 
 		}, setNewTitle);
@@ -33,16 +49,23 @@ notButton.addEventListener('click', function() {
 	if (number === 1) {
 		swal({   
 			title: "Good job!",   
-			text: "You got it right! Check out the actual <a href=" + currentArticle.url + ">article</a>!",
+			text: "You got it right! Check out the actual <a target='_blank' href=" + currentArticle.url + ">article</a>!",
 			type: "success",   
 			html: true 
 		}, setNewTitle);
 		score += 1;
+		if (highscore !== undefined) {
+			if (score > highscore) {
+				highscore = score;
+				localStorage.setItem('onion-highscore', highscore);
+				highscoreElement.innerHTML = "High Score: " + highscore;
+			}
+		}
 		scoreElement.innerHTML = score;
 	} else {
 		swal({   
 			title: "Nope.",   
-			text: "Maybe next time. Check out the actual <a href=" + currentArticle.url + ">article</a>!",
+			text: "Maybe next time. Check out the actual <a target='_blank' href=" + currentArticle.url + ">article</a>!",
 			type: "error",   
 			html: true 
 		}, setNewTitle);
@@ -67,5 +90,18 @@ function setNewTitle() {
 		    titleElement.style.display = "block";
 		    loaderElement.style.display = "none";
 		});
+	}
+}
+
+function storageAvailable(type) {
+	try {
+		var storage = window[type],
+			x = '__storage_test__';
+		storage.setItem(x, x);
+		storage.removeItem(x);
+		return true;
+	}
+	catch(e) {
+		return false;
 	}
 }
